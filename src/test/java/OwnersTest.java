64870295958d18e41c8746f565997033936a288a
DShurpik <dimashurpik@gmail.com>
2025-01-04 15:50:34 +0100

    Have added possibility to run test with owner profile and property reader

--- src/test/java/OwnersTest.java
+++ src/test/java/OwnersTest.java
@@ -2,10 +2,12 @@ import io.qameta.allure.*;
 import loader.ConfigLoader;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pageObject.basePage.BaseTest;
 
 import static confOwnerUtils.ConfigurationManager.config;
+import static utils.PropertyReader.getProperties;
 
-public class OwnersTest {
+public class OwnersTest extends BaseTest {
 
     @Step("Type {0} into 'Password' textbox")
     public static String typePassword(final String password) {
@@ -35,6 +37,28 @@ public class OwnersTest {
         System.out.println("Connecting to database: " + dbUrl);
         System.out.println("Username: " + dbUsername);
         System.out.println("Password: " + dbPassword);
+    }
+
+    @Owner("John Doe")
+    @Severity(SeverityLevel.CRITICAL)
+    @Story("Values from different property files")
+    @Test
+    public void test3() {
+        // Profiles values
+        String dbUrl = ConfigLoader.getProperty("db.url");
+        String dbUsername = ConfigLoader.getProperty("db.username");
+        String dbPassword = ConfigLoader.getProperty("db.password");
+
+        System.out.println("Connecting to database: " + dbUrl);
+        System.out.println("Profile username: " + dbUsername);
+        System.out.println("Profile password: " + dbPassword);
+
+        // Property reader values
+        String userName = getProperties().getProperty("userName");
+        String password = getProperties().getProperty("password");
+
+        System.out.println("User name from property reader: " + userName);
+        System.out.println("Password from property reader: " + password);
     }
 
 
